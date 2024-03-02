639d6234e3e28bdce0b14b9f6dbd0400b373dbfe
JustGuslav0 <galmeida.senai11@gmail.com>
2024-03-02 10:55:44 -0300

    sera q agr vai

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 function toggleMode() {
-    const body = document.body;
-    body.classList.toggle("light-mode");
+    var button = document.getElementById("modeButton");
+
+    // Toggle button color
+    button.classList.toggle("button-dark");
+    button.classList.toggle("button-light");
+    var body = document.body;
+    var icon = document.getElementById("icon");
+
+    // Toggle dark mode class on body
     body.classList.toggle("dark-mode");
+
+    // Change icon based on mode
+    if (body.classList.contains("dark-mode")) {
+        icon.classList.remove("fas", "fa-moon");
+        icon.classList.add("far", "fa-sun");
+    } else {
+        icon.classList.remove("far", "fa-sun");
+        icon.classList.add("fas", "fa-moon");
+    }
 }
